e462737787855a005c9c1c7ccd679f9b2e538eb2
AndreiAlex831 <131880722+AndreiAlex831@users.noreply.github.com>
2023-11-02 15:20:39 +0200

    fixed syntax and typos

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 
 function getComputerChoice(){
-    const words = ["Rock", "Paper", "Scissors"]
+    const words = ["Rock", "Paper", "Scissors"];
     let randomNumber = Math.floor((Math.random() * 3));
     return words[randomNumber];
 }
