fad469fbfa3b69f700c9ce51560504e44f48145d
AndreiAlex831 <131880722+AndreiAlex831@users.noreply.github.com>
2023-11-02 15:16:55 +0200

    added computer choice

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
-function getComputerChoice(rock, paper, scissors){
 
+function getComputerChoice(){
+    const words = ["Rock", "Paper", "Scissors"]
+    let randomNumber = Math.floor((Math.random() * 3));
+    return words[randomNumber];
 }
+console.log(getComputerChoice());
