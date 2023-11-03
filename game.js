47ad4664c4c65541140f9507b9d0022fd021f0e4
AndreiAlex831 <131880722+AndreiAlex831@users.noreply.github.com>
2023-11-03 09:04:44 +0200

    removed const words

--- game.js
+++ game.js
@@ -1,14 +1,14 @@
 
-function getComputerChoice(){
-    const words = ["Rock", "Paper", "Scissors"];
+function getComputerChoice(words){
+    words = ["Rock", "Paper", "Scissors"];
     let randomNumber = Math.floor((Math.random() * 3));
     return words[randomNumber];
 }
-// console.log(getComputerChoice());
+console.log(getComputerChoice());
 
 function  playRound(playerSelection, computerSelection){
 
 }
-const playerSelection = "rock";
+const playerSelection = "Rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
