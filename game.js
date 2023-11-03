6c4f776b3ba36c1d5d2433dcb3470204ffcc4dc0
AndreiAlex831 <131880722+AndreiAlex831@users.noreply.github.com>
2023-11-03 07:29:09 +0200

    added const and console output

--- game.js
+++ game.js
@@ -9,3 +9,6 @@ function getComputerChoice(){
 function  playRound(playerSelection, computerSelection){
 
 }
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
