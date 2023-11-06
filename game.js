c47040e93b8a562aa1c0f12d4f677b3498017a35
AndreiAlex831 <131880722+AndreiAlex831@users.noreply.github.com>
2023-11-06 16:31:24 +0200

    paper beats rock code

--- game.js
+++ game.js
@@ -4,11 +4,15 @@ function getComputerChoice(words){
     let randomNumber = Math.floor((Math.random() * 3));
     return words[randomNumber];
 }
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 function  playRound(playerSelection, computerSelection){
-
+       if(playerSelection.includes("Rock")
+           && computerSelection.includes("Paper")){
+           return "You Lose! Paper beats Rock";
+       }
 }
+
 const playerSelection = "Rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
