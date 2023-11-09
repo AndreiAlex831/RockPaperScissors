8be0123e5b298ad611b915c238e2349db8fe47e6
AndreiAlex831 <131880722+AndreiAlex831@users.noreply.github.com>
2023-11-09 17:04:32 +0200

    removed unnecessary console.log outputs

--- game.js
+++ game.js
@@ -3,7 +3,8 @@ function getComputerChoice(words) {
     const randomNumber = Math.floor((Math.random() * 3));
     return words[randomNumber];
 }
-
+const playerSelection = prompt("Enter your choice");
+const computerSelection = getComputerChoice();
      function playRound(playerSelection, computerSelection) {
              playerSelection = playerSelection.toLowerCase();
              computerSelection = computerSelection.toLowerCase();
@@ -17,9 +18,7 @@ function getComputerChoice(words) {
             return  "You Lose! " + computerSelection + " beats " + playerSelection;
          }
 }
-const playerSelection = prompt("Enter your choice");
-const computerSelection = getComputerChoice();
-console.log(playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1));
+console.log(playerSelection);
 console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
 
