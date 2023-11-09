b833192ab6191ce4e8eaa438513c4f63e3c0632b
AndreiAlex831 <131880722+AndreiAlex831@users.noreply.github.com>
2023-11-09 16:43:38 +0200

    new playround version

--- game.js
+++ game.js
@@ -5,25 +5,22 @@ function getComputerChoice(words) {
 }
 
      function playRound(playerSelection, computerSelection) {
-         if (playerSelection === computerSelection) {
-             return "It's a tie! Both chose " + playerSelection;
-         } else if (
-             (playerSelection === "rock" && computerSelection === "scissors") ||
+             playerSelection = playerSelection.toLowerCase();
+             computerSelection = computerSelection.toLowerCase();
+         if ((playerSelection === "rock" && computerSelection === "scissors") ||
              (playerSelection === "paper" && computerSelection === "rock") ||
-             (playerSelection === "scissors" && computerSelection === "paper")
-     ){
+             (playerSelection === "scissors" && computerSelection === "paper")) {
              return "You Win! " + playerSelection + " beats " + computerSelection;
+         } else if (playerSelection === computerSelection){
+             return " It's a tie! Both chose " + playerSelection;
          } else {
-             return "You Lose! " + computerSelection + " beats " + playerSelection;
+            return  "You Lose! " + computerSelection + " beats " + playerSelection;
          }
 }
-
-const playerSelection = "rock";
+const playerSelection = prompt("Enter your choice");
 const computerSelection = getComputerChoice();
-playerSelection.toLowerCase();
 console.log(playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1));
 console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
 
-// const playerSelection = prompt("Enter your choice");
 
