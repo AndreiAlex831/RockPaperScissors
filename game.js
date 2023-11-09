1658aca7e2810e02f418d446948c4a914657cd9e
AndreiAlex831 <131880722+AndreiAlex831@users.noreply.github.com>
2023-11-09 16:32:17 +0200

    new playround version

--- game.js
+++ game.js
@@ -1,42 +1,29 @@
-
-function getComputerChoice(words){
+function getComputerChoice(words) {
     words = ["Rock", "Paper", "Scissors"];
     const randomNumber = Math.floor((Math.random() * 3));
     return words[randomNumber];
 }
 
-function  playRound(playerSelection, computerSelection){
-       if(playerSelection.includes("rock")
-           && computerSelection.includes("Paper")){
-           return "You Lose! Paper beats Rock";
-       } else if (playerSelection.includes("rock")
-           && computerSelection.includes("Scissors")){
-           return "You win! Rock beats Scissors";
-       } else if (playerSelection.includes("paper")
-           && computerSelection.includes("Rock")){
-           return "You win! Paper beats Rock";
-       } else if (playerSelection.includes("paper")
-           && computerSelection.includes("Scissors")){
-           return "You lose! Scissors beats Paper";
-       } else if (playerSelection.includes("scissors")
-           && computerSelection.includes("Rock")){
-           return "You lose! Rock beats Scissors ";
-       }else if (playerSelection.includes("scissors")
-           && computerSelection.includes("Paper")){
-           return "You win! Scissors beats Paper ";
-       } else{
-           return "It's a Draw!"
-       }
-
+     function playRound(playerSelection, computerSelection) {
+         if (playerSelection === computerSelection) {
+             return "It's a tie! Both chose " + playerSelection;
+         } else if (
+             (playerSelection === "rock" && computerSelection === "scissors") ||
+             (playerSelection === "paper" && computerSelection === "rock") ||
+             (playerSelection === "scissors" && computerSelection === "paper")
+     ){
+             return "You Win! " + playerSelection + " beats " + computerSelection;
+         } else {
+             return "You Lose! " + computerSelection + " beats " + playerSelection;
+         }
 }
 
-   function game(){
-
-   }
-
-const playerSelection = prompt("Enter your choice");
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
 playerSelection.toLowerCase();
 console.log(playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1));
-const computerSelection = getComputerChoice();
 console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
+
+// const playerSelection = prompt("Enter your choice");
+
