be5dd5a260590a3d9e5db5fba016b38077899054
AndreiAlex831 <131880722+AndreiAlex831@users.noreply.github.com>
2024-07-08 11:15:54 +0300

    added random math

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 // Pseudocode
 //define getComputerChoice function
 function getComputerChoice (){
+//use random math to return rps
+  return Math.floor(Math.random() * 3);
 
 }
-//use random math to return rps
+console.log(getComputerChoice());
+getComputerChoice();
+
 
