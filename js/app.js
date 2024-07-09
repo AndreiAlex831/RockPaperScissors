e18883b009bceb37c9e2f9f4282adf78416370cc
AndreiAlex831 <131880722+AndreiAlex831@users.noreply.github.com>
2024-07-09 12:20:58 +0300

    added playround function

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
-// Pseudocode
-//define getComputerChoice function
 function getComputerChoice (){
-//use random math to return rps
   const computerChoice = ["Rock", "Paper", "Scissors"];
   const random = Math.floor(Math.random() * computerChoice.length);
   return computerChoice[random];
 }
-//test the function
 console.log(getComputerChoice());
 getComputerChoice();
-//define humanChoice function
 function humanChoice (){
-//use prompt to get choice
   let myChoice = prompt("Rock, Paper or Scissors?: ");
   myChoice = myChoice.charAt(0).toUpperCase() + myChoice.substring(1);
   console.log(myChoice);
 }
 humanChoice();
+const humanScore = 0;
+const computerScore = 0;
 
+function playRound (humanChoice, computerChoice) {
+
+}
 
 
 
